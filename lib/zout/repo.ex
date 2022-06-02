a5b9161cebecefa535622a36e0b57bfa6dd7622c
mcbloch <github@mcbloch.dev>
2022-06-02 03:38:59 +0200

    small cleanup

--- lib/zout/repo.ex
+++ lib/zout/repo.ex
@@ -3,12 +3,4 @@ defmodule Zout.Repo do
     otp_app: :zout,
     adapter: Ecto.Adapters.Postgres
 
-  @doc """
-  Dynamically loads the repository url from the
-  DATABASE_URL environment variable.
-  """
-  def init(_, opts) do
-    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
-  end
-
 end
