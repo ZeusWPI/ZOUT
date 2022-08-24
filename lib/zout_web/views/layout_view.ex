407c8847ad83a6d6cad1c812bb5d148bb911ff1a
Niko Strijbol <strijbol.niko@gmail.com>
2022-08-24 21:31:01 +0200

    Don't show import if not authorized
    Previously, the "Import" menu was always shown, even to users that were
    not authorized or if there was no user.

--- lib/zout_web/views/layout_view.ex
+++ lib/zout_web/views/layout_view.ex
@@ -1,6 +1,9 @@
 defmodule ZoutWeb.LayoutView do
   use ZoutWeb, :view
 
+  # Needed to check permissions in the navigation bar.
+  alias Zout.Data
+
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
