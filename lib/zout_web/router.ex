36575ee9f0aed14d9ed865275739326a1b3417ba
Niko Strijbol <strijbol.niko@gmail.com>
2022-08-27 19:14:34 +0200

    Enable Phoenix dashboard for admins

--- lib/zout_web/router.ex
+++ lib/zout_web/router.ex
@@ -1,5 +1,6 @@
 defmodule ZoutWeb.Router do
   use ZoutWeb, :router
+  import Phoenix.LiveDashboard.Router
 
   pipeline :browser do
     plug :accepts, ["html", "json"]
@@ -16,6 +17,10 @@ defmodule ZoutWeb.Router do
 
   pipeline :api do
     plug :accepts, ["html", "json"]
+  end
+
+  pipeline :admin do
+    plug :admin_only
   end
 
   scope "/", ZoutWeb do
@@ -39,21 +44,18 @@ defmodule ZoutWeb.Router do
   #   pipe_through :api
   # end
 
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
+  defp admin_only(conn, _opts) do
+    alias Zout.Data
+    user = Guardian.Plug.current_resource(conn)
+    # Ugly hack...
+    Bodyguard.permit!(Data.Policy, :is_admin, user)
+    conn
+  end
 
-    scope "/" do
-      pipe_through :browser
+  scope "/" do
+    pipe_through [:browser, :auth, :admin]
 
-      live_dashboard "/dashboard", metrics: ZoutWeb.Telemetry
-    end
+    live_dashboard "/dashboard", metrics: ZoutWeb.Telemetry
   end
 
   # Enables the Swoosh mailbox preview in development.
--- mix.exs
+++ mix.exs
@@ -48,6 +48,7 @@ defmodule Zout.MixProject do
       {:phoenix, "~> 1.6.9"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
+      {:ecto_psql_extras, "~> 0.6"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
