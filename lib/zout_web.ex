974d2889eb7f1760596a232958075145dd52bce0
Niko Strijbol <strijbol.niko@gmail.com>
2025-12-21 11:22:56 +0100

    Fix live helpers deprecation

--- lib/zout_web.ex
+++ lib/zout_web.ex
@@ -94,7 +94,7 @@ defmodule ZoutWeb do
       use PhoenixHTMLHelpers
 
       # Import LiveView and .heex helpers (live_render, live_patch, <.form>, etc)
-      import Phoenix.LiveView.Helpers
+      import Phoenix.Component
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
